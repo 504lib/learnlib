2f068eb84b8e592b5b7230d7471308c5bab69111
may-i-ask-36336_0 <2678587922@qq.com>
2025-11-12 21:25:47 +0800

    输入输出

---
+++ WYT_Lib/C_Practice2/printf.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main()
+{
+    int num = 0;
+    printf("请输入一个数:");
+    scanf("%d",&num);
+    printf("the num Hex is : 0x\n",num);
+    printf("the num Decimal is ：0d\n",num);
+    return 0;
+}
