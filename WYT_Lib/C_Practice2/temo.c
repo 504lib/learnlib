1f44c00bd91ec72cdb4670a6407aedf88906a448
may-i-ask-36336_0 <2678587922@qq.com>
2025-11-21 21:56:23 +0800

    一加到一百

---
+++ WYT_Lib/C_Practice2/temo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+// int main()
+// {
+// 	int arr[]={1,2,3,4,5,6,7,8,9,10};
+// 	int n=17;
+// 	int left = 0;
+// 	int right = sizeof(arr)/sizeof(arr[0])-1;
+// 	bool found = false;
+// 	while (left <= right)
+// 	{
+// 		int mid = (left + right)/2;
+// 		if(arr[mid]<n)
+// 	{
+// 		left = mid+1;
+// 	}
+// 	else if(arr[mid]>n)
+// 	right = mid -1;
+// 	else
+// 	{
+// 		printf("找到了，下标是:%d\n",mid);
+// 		found = true;
+// 		break;
+// 	}
+// 	}
+// 	if (found == false)
+// 	printf("找不到\n");
+// 	return 0;
+// }
+
