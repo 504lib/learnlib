ea85ca855ea4ddcdd27354e8a890db6868d61167
may-i-ask-36336_0 <2678587922@qq.com>
2025-11-22 23:02:02 +0800

    这回再错我姓倒着写

---
+++ WYT_Lib/sum.c/sum .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+uint16_t sum1To100()
+    {
+        uint16_t sum = 0;
+        for(int i=0;i<=100;i++)
+        {
+            sum +=i;
+        }
+        return sum;
+    }
+int main()
+{
+    uint16_t result = sum1To100();
+    printf("1+2+3+4+...+100=%u\n",result);
+    return 0;
+}
