c63532ee67528d2721b943a04447eef846707862
whyP762 <3046961251@qq.com>
2025-12-16 21:31:06 +0800

    补充文件

---
+++ ZK_Lib/LVGL_H7_ST7783V/build/Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,8 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "LVGL_H7_ST7783V.map"
+  )
+endif()
