8cf700eeaf8591792ef481aa06ce9d9b8c889434
whyP762 <3046961251@qq.com>
2026-07-29 14:05:53 +0800

    feat: 添加主程序文件，实现WiFi连接、摄像头图像传输及IP显示功能

---
+++ Fellow_Car_Project/2026h/2026maxicam_project/main.py
@@ -0,0 +1,41 @@
+from maix import camera, time, app, http, image, display, network, err
+
+# 1. 连手机热点
+SSID = "iQOO800"
+PASS = "Zhaokaiyyds123."
+
+w = network.wifi.Wifi()
+e = w.connect(SSID, PASS, wait=True, timeout=60)
+err.check_raise(e, "WiFi connect failed")
+ip = w.get_ip()
+
+# 2. 屏幕显示 IP
+disp = display.Display()
+bg = image.Image(disp.width(), disp.height())
+bg.draw_string(10, 10, f"IP: {ip}",
+               color=image.Color.from_rgb(255, 255, 255))
+bg.draw_string(10, 40, f"http://{ip}:8000",
+               color=image.Color.from_rgb(0, 255, 0))
+disp.show(bg)
+
+# 3. 摄像头 + 图传
+cam = camera.Camera(640, 480)
+stream = http.JpegStreamer()
+stream.start()
+print("http://{}:{}".format(stream.host(), stream.port()))
+
+# 4. 主循环
+while not app.need_exit():
+    img = cam.read()
+
+    # 球位置检测
+    # x = detect_ball(img)
+    # uart.write(f"{x}\n")
+
+    # LCD 渲染
+    disp.show(img)
+
+    # 图传推流
+    jpg = img.to_jpeg()
+    stream.write(jpg)
+    time.sleep_ms(33)
